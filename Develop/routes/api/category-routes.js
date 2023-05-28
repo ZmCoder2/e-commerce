a9bd23d6836c596103d88ed165d261bcde13d580
Zack Miller <126508376+ZmCoder2@users.noreply.github.com>
2023-05-27 21:43:13 -0400

    started the server.js and created database

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,16 +3,37 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
+  try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch {
   // be sure to include its associated Products
+    res.status(500).json(err);
+}
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
-});
+  try {
+    // be sure to include its associated Products
+    const categoryData = await Category.findByPk(re.params.id, {
+      include: [{}]
+    })
 
+    if (!categoryData) {
+      res.status(404).json({ });
+      return;
+    }
+    
+    res.status(404).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  
+});
+// where you left off.
 router.post('/', (req, res) => {
   // create a new category
 });
--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+const mysql = require('mysql2')
 // import sequelize connection
 
 const app = express();
@@ -7,6 +8,12 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const db =mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: '',
+})
 
 app.use(routes);
 
